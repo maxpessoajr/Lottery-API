e3756f6be64abe423b4712f36db95988a6351086
Pietro Caselani <pc1992@gmail.com>
2017-07-11 13:04:20 -0300

    First commit

---
+++ src/main/java/com/devops/lottery/api/HelloController.java
@@ -0,0 +1,36 @@
+package com.devops.lottery.api;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * Created by Pietro Caselani on 11/07/17.
+ */
+@RestController
+public final class HelloController {
+
+	private final Random random = new Random(System.currentTimeMillis());
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello cuzão";
+	}
+
+	@RequestMapping(value = "/lottery", method = RequestMethod.POST)
+	public String lottery(@RequestBody Map<String, List<String>> jsonNames) {
+		final List<String> names = jsonNames.get("names");
+
+		final int size = names.size();
+
+		final int selected = random.nextInt(size);
+
+		return names.get(selected);
+	}
+
+}
